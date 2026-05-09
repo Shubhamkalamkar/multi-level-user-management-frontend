e3a0e9397afafd5342a7b16d72dbde0791ae751b
shubham kalamkar <shubhamkalamkar0101@gmail.com>
2026-05-09 22:45:26 +0530

    fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://multi-level-user-management-backend.onrender.com/api',
-  socketUrl: 'https://multi-level-user-management-backend.onrender.com'
+  apiUrl: '/api',
+  socketUrl: '/'
 };
